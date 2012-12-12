1f7386d18cbfff5d03c58bd3639ee0ced35b6ec9
Bartosz Domagala <quetz05@gmail.com>
2012-12-12 21:05:40 +0100

    implementacja klasy vector
    Mam problem z wyświetlaniem doubli w c++ (zamienia mi na inty ciągle),
    nie wiem czemu, ale formalnie powinno się wszystko zgadzać

--- vector.cpp
+++ vector.cpp
@@ -1,14 +1,50 @@
-#include <iostream>
-
-int main()
-{
+#include <math.h>
 
 
-std::cout<<"Hello GIT"<<std::endl;
+//klasa wektor
+class Vector3{
 
-   return 0;
+    public:
 
-}
+    //konstruktor
+    Vector3(int a, int b, int c){ x=a; y=b; z=c;};
+
+
+    //przeciazenia operatorow
+    Vector3 operator +(const Vector3 &vector){return Vector3(this->x + vector.x, this->y + vector.y, this->z + vector.z );};
+    Vector3 operator -(const Vector3 &vector){return Vector3(this->x - vector.x, this->y - vector.y, this->z - vector.z );};
+    Vector3 operator *(double value){return Vector3(this->x *value, this->y *value, this->z *value);};
+    Vector3 operator /(double value){return Vector3(this->x /value, this->y /value, this->z /value);};
+
+
+    //iloczyn skalarny:
+    double dot(Vector3 vector){return (this->x * vector.x + this->y * vector.y + this->z * vector.z);};
+    //iloczyn wektorowy:
+    Vector3 cross(Vector3 vector){return Vector3(this->y * vector.z - this->z * vector.y, this->z * vector.x - this->x * vector.z, this->x * vector.y - this->y * vector.x);}
+
+
+    //zwroc dlugosc wektora
+    double getLength(){return sqrt(x*x+y*y+z*z);};
+    //zwroc pierwiatek dlugosci wektora
+    double getLengthSq(){return x*x+y*y+z*z;};
+    //normalizacja wektora
+    Vector3 normalized(){return (*this)/(this->getLength());};
 
 
 
+    //metody do zwracania wspolrzednych wektora
+    double getX(){return x;};
+    double getY(){return y;};
+    double getZ(){return z;};
+    //metody do ustawiania wspolrzednych wektora
+    void setX(double i) {x = i;};
+    void setY(double i) {y = i;};
+    void setZ(double i) {z = i;};
+
+    private:
+
+    double x;
+    double y;
+    double z;
+
+};
