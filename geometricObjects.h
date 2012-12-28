8d692903d48d89838adcf10922fc9a682b20014b
Bartosz Domagala <quetz05@gmail.com>
2012-12-28 13:13:26 +0100

    Kuźwa jak mi się nie chce...
    ...ale się biorę za instalację QT, bo jak zwykle CodeBlocksowi coś nie
    pasuje. Może z 5 będzie łatwiej...

--- geometricObjects.h
+++ geometricObjects.h
@@ -1,9 +1,38 @@
 #include "ray.h"
+#include <QColor>
 
-class GeometricObject
-{
+
+class GeometricObject {
+
     public:
-    //@do dodania funkcja koloru z QT
+
     virtual bool HitTest(Ray ray, double &distance);
+    QColor getColor(){return color;};
+    void setColor(QColor newColor){color = newColor;};
+
+
+
+
+    private:
+
+    QColor color;
+
+};
+
+class Sphere: GeometricObject {
+
+    public:
+
+    Sphere(Vector3 newCenter, double newRadius, QColor newColor)
+    {
+        center = newCenter;
+        radius = newRadius;
+        color = newColor;
+    }
+
+    bool HitTest(Ray ray, double &distance)
+    {
+        //@to do
+    }
 
 };
