3cfe931fb5858f43cf38a5e1b22eb92a9704e69f
Bartosz Domagala <quetz05@gmail.com>
2013-01-04 20:22:58 +0100

    obiekty3

--- geometric_objects.cpp
+++ geometric_objects.cpp
@@ -1,16 +1,53 @@
+/**Plik źródłowy zawierający definicje funkcji klas obiektów geometrycznych*/
+
 #include "geometric_objects.h"
 
+/**definicja funkcji getColor obiektu*/
 QColor Geometric_object::getColor(){
     return color;
 }
 
+/**definicja funkcji setColor obiektu*/
 void Geometric_object::setColor(QColor newColor){
     color = newColor;
 }
 
+/**definicja konstruktora klasy Sphere (Kuli)*/
 Sphere::Sphere(Vector3 newCenter, double newRadius, QColor newColor){
     center = newCenter;
     radius = newRadius;
     color = newColor;
+}
+
+/**definicja funkcji HitTest*/
+bool const Sphere::HitTest(VRay ray, double &minDistance){
+
+    double t;
+    Vector3 distance = ray.Origin - center;
+
+    double a = ray.Direction.LengthSq;
+    double b = (distance * 2).Dot(ray.Direction);
+    double c = distance.LengthSq - radius * radius;
+
+    double disc = b * b - 4 * a * c;
+
+    if (disc < 0)
+        return false;
+
+    double discSq = Math.Sqrt(disc);
+
+    double denom = 2 * a;
+
+    t = (-b - discSq) / denom;
+
+    if (t < Ray.Epsilon)
+        t = (-b + discSq) / denom;
+
+    if (t < Ray.Epsilon)
+        return false;
+
+    minDistance = t;
+
+    return true;
 }
 
--- geometric_objects.h
+++ geometric_objects.h
@@ -1,37 +1,42 @@
+/**nagłówek zawierający deklaracje klas figur geoemtrycznych oraz ich metod i zmiennych*/
 #include "ray.h"
 #include "vector.h"
 #include <QColor>
 
-
+/**klasa bazowa figury geometrycznej*/
 class Geometric_object {
 
     public:
 
-    virtual bool HitTest(VRay ray, double &distance);
-    QColor getColor();
-    void setColor(QColor newColor);
-
-
-
+        /**funkcja sprawdzająca trafienie promienia w figurę*/
+        virtual bool HitTest(VRay ray, double &distance);
+        /**funkcja zwracająca kolor figury*/
+        QColor getColor();
+        /**funkcja ustawiająca kolor figury*/
+        void setColor(QColor newColor);
 
     protected:
 
-    QColor color;
+        //kolor figury
+        QColor color;
 
 };
 
+/**klasa pochodna figury opisująca sferę (kulę)*/
 class Sphere: Geometric_object {
 
     public:
 
-    Sphere(Vector3 newCenter, double newRadius, QColor newColor);
+        Sphere(Vector3 newCenter, double newRadius, QColor newColor);
 
 
-    bool HitTest(VRay ray, double &distance);
+    bool const HitTest(VRay ray, double &minDistance);
 
     private:
 
+    /**środek kuli*/
     Vector3 center;
+    /**promień*/
     double radius;
 
 };
