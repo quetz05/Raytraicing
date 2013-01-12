632d39f9fdee4f918a01401114f0fd141bcde7fd
Bartosz Domagala <quetz05@gmail.com>
2013-01-12 13:03:39 +0100

    Pedanteria: 1/3 nagłówku materials.h

--- hit_info.h
+++ hit_info.h
@@ -1,23 +1,33 @@
+/**plik nagłówkowy zawierający implementację klasy HitInfo*/
 #ifndef HIT_INFO_H
 #define HIT_INFO_H
-
-
 #include "vector.h"
 #include "geometric_objects.h"
 
+/**deklaracja klasy World*/
 class World;
 
+/**klasa HitInfo przechowująca informacje o trafieniach promieni*/
 class HitInfo{
-public:
 
-    //~HitInfo(){delete world; delete hit_object;}
-    HitInfo(){hit_object=NULL; world=NULL;}
-    GeometricObject *hit_object;
-    Vector3 normal;
-    Vector3 hit_point;
-    VRay ray;
-    World *world;
-    int depth;
+    public:
+
+        /**zmienna odpowiadająca za głębię*/
+        int depth;
+        /**konstruktor bezargumentowy tworzący wskaźniki klasy wskazujące na NULL*/
+        HitInfo(){hit_object=NULL; world=NULL;}
+        /**destruktor klasy HitInfo*/
+        //~HitInfo(){delete hit_object; delete world;}
+        /**zmienna określająca normalną*/
+        Vector3 normal;
+        /**zmienna określająca punkt trafienia*/
+        Vector3 hit_point;
+        /**zmienna określająca promień*/
+        VRay ray;
+        /**wskaźnik na świat*/
+        World *world;
+        /**wskaźnik na obiekt geometryczny*/
+        GeometricObject *hit_object;
 
 };
 
--- materials.h
+++ materials.h
@@ -1,79 +1,85 @@
+/**nagłówek zawierający deklarację klas i funkcji związanych z materiałem*/
 #ifndef MATERIAL_H
 #define MATERIAL_H
 #include <QColor>
 #include "point_light.h"
 #include "mycolor.h"
 
+/**deklaracja klasy HitInfo*/
 class HitInfo;
+/**deklaracja klasy World*/
 class Raytracer;
 
-/**klasa abstrakcyjna opisujÄ…ca materiaĹ‚*/
+/**klasa abstrakcyjna opisująca materiał*/
 class Material{
-public:
-
-    /**metoda tworzÄ…ca blask materiaĹ‚u*/
-    virtual MyColor radiance(PointLight,  HitInfo&)=0;
-    virtual MyColor shade( Raytracer &tracer, HitInfo &hit)=0;
-
-};
-
-/**klasa reprezentujÄ…ca materiaĹ‚ idealnie rozpraszajÄ…cy*/
-class PerfectDiffuse: public Material{
-
     public:
 
-        /**konstruktor tworzÄ…cy materiaĹ‚ danego koloru*/
-        PerfectDiffuse(MyColor c): color(c){}
-        /**przeciÄ…ĹĽona metoda tworzÄ…ca blask materiaĹ‚u*/
-        MyColor radiance(PointLight, HitInfo &);
+        /**metoda wirtualna tworząca blask materiału*/
+        virtual MyColor radiance(const PointLight, HitInfo&)=0;
+        /**metoda wirtualna tworząca cień materiału*/
+        virtual MyColor shade(Raytracer &tracer, HitInfo &hit)=0;
 
-        MyColor shade(Raytracer &tracer, HitInfo &hit);
+    protected:
 
-
-
-    private:
-
-        /**kolor materiaĹ‚u*/
+        /**kolor materiału*/
         MyColor color;
 
 };
 
-/**klasa reprezentujÄ…ca materiaĹ‚ Phonga*/
+/**klasa reprezentująca materiał idealnie rozpraszający*/
+class PerfectDiffuse: public Material{
+
+    public:
+
+        /**konstruktor tworzący materiał danego koloru*/
+        PerfectDiffuse(MyColor c){color=c;}
+        /**przeciążona metoda tworząca blask materiału idealnie rozpraszającego*/
+        MyColor radiance(const PointLight, HitInfo &);
+        /**przeciążona metoda tworząca cień materiału idealnie rozpraszającego*/
+        MyColor shade(Raytracer &tracer, HitInfo &hit);
+
+
+};
+
+/**klasa reprezentująca materiał Phonga*/
 class Phong : public Material{
 
     public:
 
-        /**konstruktor klasy*/
+        /**konstruktor klasy bezargumentowy (przypisujący polom obiektu wartości 0)*/
         Phong();
-        Phong(MyColor, double, double,double);
-        MyColor radiance(PointLight light, HitInfo&);
+        /**konstruktor klasy tworzący materiał)*/
+        Phong(MyColor, double, double, double);
+        MyColor radiance(const PointLight, HitInfo&);
         double phong_factor(Vector3, Vector3 , Vector3);
 
         MyColor shade(Raytracer &tracer, HitInfo &hit);
 
          private:
-             /**kolor materiaĹ‚u*/
-             MyColor color;
+
+             /**współczynnik dyfuzji*/
              double diffuse_coeff;
+             /**rozbłysk*/
              double specular;
+             /**wykładnik rozbłysku*/
              double specular_exponent;
 };
 
 
-/**klasa reprezentujÄ…ca materiaĹ‚ modelujÄ…cy odbicie*/
+/**klasa reprezentująca materiał modelujący odbicie*/
 class Reflective : public Material{
 
     public:
 
         /**konstruktor klasy*/
         Reflective(MyColor, double, double,double, double);
-        MyColor radiance(PointLight light, HitInfo &);
+        MyColor radiance(const PointLight, HitInfo &);
 
         MyColor shade(Raytracer &tracer, HitInfo &hit);
 
          private:
-             /**kolor materiaĹ‚u*/
-             MyColor color;
+             /**kolor materiału*/
+            // MyColor color;
              double reflectivity;
              Phong direct;
 };
