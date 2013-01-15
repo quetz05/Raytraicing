c1f768723e9c480d8de6325190ddb022de2ab2e6
quetz05 <quetz05@gmail.com>
2013-01-15 23:24:41 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,14 +9,41 @@
 int main(int argc, char *argv[])
 {
 
+    World world(QColor(0,180,180));
 
-    // world.add(new Plane(Vector3(0,-2,0),Vector3(0,1,0),ref_gray_mat));
-    // world.add_light(PointLight(Vector3(0,5,-5), MyColor(0.5,0.5,0.5), &distributor,0.5));
+    int samples = 1;
+
+    Material *material = new Reflective (QColor(2,255,255), 0.4, 1, 300, 0.6);
+    PinholeCamera camera(Vector3(0,0,0),Vector3(0,0,1),Vector3(0,-1,0),Vector2(1,0.75),1);
+
+
+    world.add(new Sphere(Vector3(-2,-1.15,1), 2,  material));
+    world.add(new Sphere(Vector3(2,-1.15,1), 2,  material));
+    world.add(new Sphere(Vector3(0,3.45-1.15,1), 2,  material));
+    world.add(new Sphere(Vector3(0,1.15-1.15,3.45+1), 2,  material));
+
+    JitteredGenerator gener(0);
+    SquareDistributor dist;
+    Sampler distributor(gener,dist,samples,60,0);
+
+
+
+
+    world.add_light(PointLight(Vector3(0,0,3.45-2), MyColor(1,1,1)));
+
+    Raytracer tracer(5);
+    QImage image;
+    image = tracer.ray_trace(world, camera, QSize(800, 600), &distributor);
+
+
+
 
     QApplication a(argc, argv);
     MainWindow w;
 
-    w.new_world();
+    w.copy_world(image, world);
+    w.show_image();
+   // w.new_world();
     w.show();
     
     return a.exec();
